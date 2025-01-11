81828cbdfbed99f43be8ee74645c88e5bb04acfe
Enock Bezerra Ferreira de Souza <enock.souza@ccc.ufcg.edu.br>
2025-01-11 14:52:28 -0300

    Login screen update: small visual changes

--- tm_front/lib/screens/login_screen.dart
+++ tm_front/lib/screens/login_screen.dart
@@ -32,186 +32,207 @@ class LoginScreenState extends State<LoginScreen>
       body: Padding
       (
         padding: const EdgeInsets.all(16.0),
-        child: Form
+        child: Column
         (
-          key: formKey,
-          child: Column
-          (
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: 
-            [
-              // const CircleAvatar
-              // (
-              //   radius: 56,
-              //   backgroundColor: Color(0xFFBB86FC),
-              //   child: Text('LOGO', style: TextStyle(color: Colors.black)),
-              //),
-              const SizedBox(height: 24),
-              const Column
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: 
+          [
+            Form
+            (
+              key: formKey,
+              child: Column
               (
-                children:
-                [
-                  Text
-                  (
-                    'Taverna',
-                    textAlign: TextAlign.center,
-                    style: TextStyle
-                    (
-                      color: Color(0xFFBB86FC),
-                      fontSize: 48,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'BerkshireSwash'
-                    ),
-                  ),
-                  Text
-                  (
-                    'Multiversal',
-                    textAlign: TextAlign.center,
-                    style: TextStyle
-                    (
-                      color: Color(0xFFBB86FC),
-                      fontSize: 48,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'BerkshireSwash'
-                    )
-                  )
-                ]
-              ),
-              const SizedBox(height: 24),
-              TextFormField
-              (
-                decoration: InputDecoration
-                (
-                  hintText: 'Digite o e-mail',
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                  filled: true,
-                  fillColor: const Color(0xFF312B47),
-                  prefixIcon: const Icon(Icons.email, color: Color(0xFFB3B3B3))
-                ),
-                style: const TextStyle(color: Colors.white),
-                validator: (value)
-                {
-                  if (value == null || value.isEmpty)
-                  {
-                    return 'Falta o e-mail';
-                  }
-                  if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value))
-                  {
-                    return 'E-mail inválido';
-                  }
-                  return null;
-                }
-              ),
-              const SizedBox(height: 16),
-              TextFormField
-              (
-                obscureText: obscureTextVar,
-                decoration: InputDecoration
-                (
-                  hintText: 'Digite a senha',
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-                  filled: true,
-                  fillColor: const Color(0xFF322B47),
-                  prefixIcon: const Icon(Icons.lock, color: Color(0xFFB3B3B3)),
-                  suffixIcon: IconButton
-                  (
-                    icon: Icon
-                    (
-                      obscureTextVar ? Icons.visibility_off : Icons.visibility,
-                      color: const Color(0xFFB3B3B3)
-                    ),
-                    onPressed: togglePasswordVisibility
-                  )
-                ),
-                style: const TextStyle(color: Colors.white),
-                validator: (value)
-                {
-                  if (value == null || value.isEmpty)
-                  {
-                    return 'Falta a senha';
-                  }
-                  return null;
-                }
-              ),
-              const SizedBox(height: 8),
-              TextButton
-              (
-                onPressed: () 
-                {
-                  print('Esqueci minha senha'); //TODO: delete this line
-                },
-                child: const Text
-                (
-                  'Esqueci minha senha', 
-                  style: TextStyle
-                  (
-                    color: Color(0xFF03DAC6),
-                    decoration: TextDecoration.underline,
-                    decorationColor: Color(0xFF03DAC6),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-              Row
-              (
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: 
                 [
-                  const Icon(Icons.login, color: Color(0xFFB3B3B3)),
-                  const SizedBox(width: 10),
-                  Expanded
+                  // const CircleAvatar
+                  // (
+                  //   radius: 56,
+                  //   backgroundColor: Color(0xFFBB86FC),
+                  //   child: Text('LOGO', style: TextStyle(color: Colors.black)),
+                  //),
+                  const SizedBox(height: 24),
+                  const Column
                   (
-                    child: SizedBox
-                    (
-                      height: 50,
-                      child: CustomButton
+                    children:
+                    [
+                      Text
                       (
-                        text: 'Entrar',
-                        onPressed: () 
-                        {
-                          if (formKey.currentState!.validate())
-                          {
-                            // TODO: Implementar ação de login
-                            print('Entrar');
-                          }
-                        }
+                        'Taverna',
+                        textAlign: TextAlign.center,
+                        style: TextStyle
+                        (
+                          color: Color(0xFFBB86FC),
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'BerkshireSwash'
+                        ),
+                      ),
+                      Text
+                      (
+                        'Multiversal',
+                        textAlign: TextAlign.center,
+                        style: TextStyle
+                        (
+                          color: Color(0xFFBB86FC),
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'BerkshireSwash'
+                        )
                       )
-                    )
+                    ]
                   ),
-                  const SizedBox(width: 10),
-                  const SizedBox(width: 24)
-                ]
+                  const SizedBox(height: 24),
+                  TextFormField
+                  (
+                    decoration: InputDecoration
+                    (
+                      hintText: 'Digite o e-mail',
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+                      filled: true,
+                      fillColor: const Color(0xFF312B47),
+                      prefixIcon: const Icon(Icons.email, color: Color(0xFFB3B3B3))
+                    ),
+                    style: const TextStyle(color: Colors.white),
+                    validator: (value)
+                    {
+                      if (value == null || value.isEmpty)
+                      {
+                        return 'Falta o e-mail';
+                      }
+                      if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value))
+                      {
+                        return 'E-mail inválido';
+                      }
+                      return null;
+                    }
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField
+                  (
+                    obscureText: obscureTextVar,
+                    decoration: InputDecoration
+                    (
+                      hintText: 'Digite a senha',
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+                      filled: true,
+                      fillColor: const Color(0xFF322B47),
+                      prefixIcon: const Icon(Icons.lock, color: Color(0xFFB3B3B3)),
+                      suffixIcon: IconButton
+                      (
+                        icon: Icon
+                        (
+                          obscureTextVar ? Icons.visibility_off : Icons.visibility,
+                          color: const Color(0xFFB3B3B3)
+                        ),
+                        onPressed: togglePasswordVisibility
+                      )
+                    ),
+                    style: const TextStyle(color: Colors.white),
+                    validator: (value)
+                    {
+                      if (value == null || value.isEmpty)
+                      {
+                        return 'Falta a senha';
+                      }
+                      return null;
+                    }
+                  ),
+                  const SizedBox(height: 8),
+                  TextButton
+                  (
+                    onPressed: () 
+                    {
+                      print('Esqueci minha senha'); //TODO: delete this line
+                    },
+                    child: const Text
+                    (
+                      'Esqueci minha senha', 
+                      style: TextStyle
+                      (
+                        color: Color(0xFF03DAC6),
+                        decoration: TextDecoration.underline,
+                        decorationColor: Color(0xFF03DAC6),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Row
+                  (
+                    children: 
+                    [
+                      const Icon(Icons.login, color: Color(0xFFB3B3B3)),
+                      const SizedBox(width: 8),
+                      Expanded
+                      (
+                        child: SizedBox
+                        (
+                          height: 50, // Defina a altura desejada aqui
+                          child: CustomButton
+                          (
+                            text: 'Entrar',
+                            textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            onPressed: () 
+                            {
+                              if (formKey.currentState!.validate())
+                              {
+                                // TODO: Implementar ação de login
+                                print('Entrar');
+                              }
+                            },
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      const SizedBox(width: 24), // Espaço reservado para o ícone oculto
+                    ],
+                  ),
+                  const SizedBox(height: 18),
+                  Row
+                  (
+                    children:
+                    [
+                      const Icon(Icons.person, color: Color(0xFFB3B3B3)),
+                      const SizedBox(width: 8),
+                      Expanded
+                      (
+                        child: SizedBox
+                        (
+                          height: 50, // Defina a altura desejada aqui
+                          child: CustomButton
+                          (
+                            text: 'Criar conta',
+                            textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            onPressed: () 
+                            {
+                              // TODO: Navegar para tela de cadastro
+                              print('Criar conta');
+                            }
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      const SizedBox(width: 24), // Espaço reservado para o ícone oculto
+                    ],
+                  ),
+                ],
               ),
-              const SizedBox(height: 15),
-              Row
+            ),
+            const Padding
+            (
+              padding: EdgeInsets.only(bottom: 16.0),
+              child: Center // Usando Center para centralizar o texto
               (
-                children:
-                [
-                  const Icon(Icons.person, color: Color(0xFFB3B3B3)),
-                  const SizedBox(width: 10),
-                  Expanded
-                  (
-                    child: SizedBox
-                    (
-                      height: 50,
-                      child: CustomButton
-                      (
-                        text: 'Criar conta',
-                        onPressed: ()
-                        {
-                          // TODO: Navegar para tela de cadastro
-                          print('Criar conta');
-                        }
-                      )
-                    )
-                  ),
-                  const SizedBox(width:10),
-                  const SizedBox(width: 24),
-                ]
-              )
-            ]
-          )
-        )
-      )
+                child: Text
+                (
+                  '© 2024 Taverna Multiversal. Todos os direitos reservados.',
+                  style: TextStyle(color: Colors.white, fontSize: 12),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- tm_front/lib/widgets/custom_button.dart
+++ tm_front/lib/widgets/custom_button.dart
@@ -1,35 +1,33 @@
 import 'package:flutter/material.dart';
 
-class CustomButton extends StatelessWidget 
-{
+class CustomButton extends StatelessWidget {
   final String text;
+  final TextStyle? textStyle;
   final VoidCallback onPressed;
   final Color backgroundColor;
   final Color textColor;
 
-  const CustomButton
-  (
-    {
-      super.key, 
-      required this.text,
-      required this.onPressed,
-      this.backgroundColor = const Color(0xFF7B0AAF),
-      this.textColor = const Color(0xFF00C925)
-    }
-  );
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.textStyle,
+    required this.onPressed,
+    this.backgroundColor = const Color(0xFF7B0AAF),
+    this.textColor = const Color(0xFF00C925),
+  });
 
   @override
-  Widget build(BuildContext context) 
-  {
-    return ElevatedButton
-    (
+  Widget build(BuildContext context) {
+    return ElevatedButton(
       onPressed: onPressed,
-      style: ElevatedButton.styleFrom
-      (
-        backgroundColor: backgroundColor, // Cor do retângulo
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor, // Cor de fundo
         foregroundColor: textColor, // Cor do texto
       ),
-      child: Text(text),
+      child: Text(
+        text,
+        style: textStyle ?? TextStyle(color: textColor),
+      ),
     );
   }
 }
