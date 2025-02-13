bc6e9cc8bace0807d5a7a615e014a1b4be0cdd7a
enock21 <enock.souza@ccc.ufcg.edu.br>
2025-02-13 12:01:58 -0300

    header: layout fixed

--- tm_front/lib/widgets/header.dart
+++ tm_front/lib/widgets/header.dart
@@ -4,33 +4,41 @@ import 'package:tm_front/widgets/tm_buttons.dart';
 
 class Header extends StatelessWidget {
   final String title;
+  //final double horizontalSpacing; // Unecessary?
 
-  const Header({required this.title, super.key});
+  const Header({
+    required this.title,
+    //this.horizontalSpacing = 1.0, // Unecessary?
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100, // Defina uma altura adequada para o cabeçalho
-      child: Stack(
+      height: 100,
+      padding: EdgeInsets.symmetric(/*horizontal: horizontalSpacing*/),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start, // Alinha tudo à esquerda
+        mainAxisSize: MainAxisSize.min, // Mantém o tamanho necessário
         children: [
-          Positioned(
-            left: 16,
-            top: 16,
-            child: TMBackButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
+          Row(
+            children: [
+              TMBackButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
           ),
-          const SizedBox(height: 50),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 16.0),
-              child: Text(
+          const SizedBox(height: 10), // Espaçamento configurável vertical
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
                 title,
                 style: AppTexts.headlineMedium,
               ),
-            ),
+            ],
           ),
         ],
       ),
