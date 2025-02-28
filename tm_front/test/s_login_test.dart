4dd691005a46a40ac75bb79cbc2298881e1dc62c
enock21 <enock.souza@ccc.ufcg.edu.br>
2025-02-28 16:40:42 -0300

    issue #5: testes ára campo de email vazio, campo de senha vazio e formato de e-mail inválido

--- tm_front/test/s_login_test.dart
+++ tm_front/test/s_login_test.dart
@@ -4,15 +4,15 @@ import 'package:tm_front/screens/s_login.dart';
 
 void main() {
   group('Tela de Login - Validações', () {
-    // testWidgets('Exibe erro quando o e-mail é inválido', (WidgetTester tester) async {
-    //   await tester.pumpWidget(const MaterialApp(home: SLogin()));
+    testWidgets('Exibe erro quando o formato do e-mail é inválido', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: SLogin()));
       
-    //   await tester.enterText(find.byType(TextFormField).first, 'email_invalido');
-    //   await tester.tap(find.text('Entrar'));
-    //   await tester.pump();
+      await tester.enterText(find.byType(TextFormField).first, 'email_invalido.com');
+      await tester.tap(find.text('Entrar'));
+      await tester.pump();
       
-    //   expect(find.text('E-mail inválido'), findsOneWidget);
-    // });
+      expect(find.text('E-mail inválido'), findsOneWidget);
+    });
 
     testWidgets('Exibe erro quando o campo de e-mail está vazio', (WidgetTester tester) async {
       await tester.pumpWidget(const MaterialApp(home: SLogin()));
@@ -23,13 +23,13 @@ void main() {
       expect(find.text('Falta o e-mail'), findsOneWidget);
     });
 
-    // testWidgets('Exibe erro quando o campo de senha está vazio', (WidgetTester tester) async {
-    //   await tester.pumpWidget(const MaterialApp(home: SLogin()));
+    testWidgets('Exibe erro quando o campo de senha está vazio', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: SLogin()));
       
-    //   await tester.tap(find.text('Entrar'));
-    //   await tester.pump();
+      await tester.tap(find.text('Entrar'));
+      await tester.pump();
       
-    //   expect(find.text('Falta a senha'), findsOneWidget);
-    // });
+      expect(find.text('Falta a senha'), findsOneWidget);
+    });
   });
 }
